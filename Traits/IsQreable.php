18685cdc78d91ecaefadb953db0af9e51b13e488
Cristian Marin <marin101998@gmail.com>
2023-11-14 11:01:39 -0500

    +add | trait IsQreable with relation

--- Traits/IsQreable.php
+++ Traits/IsQreable.php
@@ -6,16 +6,20 @@ use Illuminate\Support\Str;
 
 trait IsQreable
 {
-  public function generateQr()
+  public function generateQr($params = null)
   {
     $QrRepository = app('Modules\Iqreable\Repositories\QrRepository');
     $data = [
       'content' => $this->url,
       'entity_type' => $this->entity,
       'entity_id' => $this->id,
-      'zone' => 'mainqr'
+      'zone' => $params['zone'] ?? 'mainqr'
     ];
     //Create and return the Qr
     return $QrRepository->create($data);
+  }
+  public function qr()
+  {
+    return $this->hasMany('Modules\Iqreable\Entities\Qr', 'entity');
   }
 }
